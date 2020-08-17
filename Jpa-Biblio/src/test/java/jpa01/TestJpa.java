244eded456cf8d38961cf5e83a4a5c4883a139cf
Rachel BANDIERA <rachel.bandiera@gmail.com>
2020-08-17 20:31:07 +0200

    on y arrive

--- Jpa-Biblio/src/test/java/jpa01/TestJpa.java
+++ Jpa-Biblio/src/test/java/jpa01/TestJpa.java
@@ -9,7 +9,7 @@ import javax.persistence.TypedQuery;
 
 import org.junit.Before;
 import org.junit.Test;
-import entity.livre;
+import entity.*;
 
 public class TestJpa 
 {
@@ -28,7 +28,7 @@ public class TestJpa
 		
 		if(em != null)
 		{
-			livre livre1 = em.find(livre.class,1);
+			client livre1 = em.find(client.class,1);
 			System.out.println(livre1);
 			
 		}
@@ -39,146 +39,7 @@ public class TestJpa
 	}
 
 	
-	
-	@Test
-	public void listeLivres()
-	{
-		EntityManager em = factory.createEntityManager();
-		
-		if(em != null)
-		{
-			String query = "SELECT l FROM livre l";
-			TypedQuery<livre> q = em.createQuery(query, livre.class);
-			for(livre l : q.getResultList())
-			{
-				System.out.println(l);
-			}
-			
-		}
-		
-		em.close();
-		factory.close();
-		
-	}
-	
-	@Test
-	public void listeLivresW()
-	{
-		EntityManager em = factory.createEntityManager();
-		
-		if(em != null)
-		{
-			String query = "SELECT l FROM livre l where l.id >=3";
-			TypedQuery<livre> q = em.createQuery(query, livre.class);
-			for(livre l : q.getResultList())
-			{
-				System.out.println(l);
-			}
-			
-		}
-		
-		em.close();
-		factory.close();
-		
-	}
-	
-	@Test
-	public void listeTP02Titre()
-	{
-		EntityManager em = factory.createEntityManager();
-		
-		if(em != null)
-		{
-			String query = "SELECT l FROM livre l where l.titre = 'Germinal'";
-			TypedQuery<livre> q = em.createQuery(query, livre.class);
-			for(livre l : q.getResultList())
-			{
-				System.out.println(l);
-			}
-			
-		}
-		
-		em.close();
-		factory.close();
-		
-	}
-	
-	@Test
-	public void listeTP02Auteur()
-	{
-		EntityManager em = factory.createEntityManager();
-		
-		if(em != null)
-		{
-			String query = "SELECT l FROM livre l where l.auteur = 'Gaston Pouet'";
-			TypedQuery<livre> q = em.createQuery(query, livre.class);
-			for(livre l : q.getResultList())
-			{
-				System.out.println(l);
-			}
-			
-		}
-		
-		em.close();
-		factory.close();
-		
-	}
-	
-	@Test
-	public void insertLivre()
-	{
-		EntityManager em = factory.createEntityManager();
-		livre l = new livre();
-		l.setId(6);
-		l.setAuteur("Victor Hugo");
-		l.setTitre("Les Misérables");
-		
-		em.getTransaction().begin();
-		em.persist(l);
-		em.getTransaction().commit();
-		
-		em.close();
-		factory.close();
-		
-		
-	}
-	
-	@Test
-	public void updateLivre()
-	{
-		EntityManager em = factory.createEntityManager();
 
-		em.getTransaction().begin();
-		
-		livre l = em.find(livre.class,5);
-		l.setTitre("Du plaisir dans la cuisine");
-		em.merge(l);
-		em.getTransaction().commit();
-		
-		em.close();
-		factory.close();
-		
-		
-	}
-	
-	@Test
-	public void deleteLivre()
-	{
-		EntityManager em = factory.createEntityManager();
-
-		em.getTransaction().begin();
-		
-		livre l = em.find(livre.class,3);
-		
-		em.remove(l);
-		em.getTransaction().commit();
-		
-		em.close();
-		factory.close();
-		
-		
-	}
-	
 	
 
 }
